3bdda8978aadaa3ca6b9637f4fb42670e026ace1
Viktor Churikov <scanlave@gmail.com>
2021-09-22 18:00:47 +0800

    Initial Commit

---
+++ RandomAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomAnimation
+//
+//  Created by Виктор Чуриков on 22.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
